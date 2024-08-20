c7a3609bd604e12e18401191aec1f1644deff80b
Rahidul Islam <rahiseli@outlook.com>
2024-08-20 11:15:30 +0600

    a manager added for creating user account

--- user_account/admin.py
+++ user_account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from user_account.models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','email','username','first_name','last_name',]
--- user_account/models.py
+++ user_account/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from user_account.managers import UserManager
 
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
-    username=None
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=[]
+    username = None
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
