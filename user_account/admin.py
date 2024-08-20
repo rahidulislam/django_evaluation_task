03a72fdb42aeb268e457c68bbf3f72b0d868da0e
Rahidul Islam <rahiseli@outlook.com>
2024-08-20 12:57:15 +0600

    access token lifetime is changed

--- user_account/admin.py
+++ user_account/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from user_account.models import User
 # Register your models here.
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id','email','first_name','last_name','date_joined']
+    list_display = ["id", "email", "first_name", "last_name", "date_joined"]
