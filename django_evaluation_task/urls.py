b49918bf6fbc94fe79b6ae6191dcd98ed340a7f1
Rahidul Islam <rahiseli@outlook.com>
2024-08-20 13:01:04 +0600

    root url is updated

--- django_evaluation_task/urls.py
+++ django_evaluation_task/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('api/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/v1/auth/', include('user_account.urls')),
 ]
